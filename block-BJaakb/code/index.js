8beef965bcf41798e0892bae74c74dc1167736a1
sohailshaik8328 <sohailshaik765@gmail.com>
2021-06-26 11:42:44 +0530

    asfja

--- block-BJaakb/code/index.js
+++ block-BJaakb/code/index.js
@@ -1,43 +1,40 @@
 let input = document.querySelector("input");
 let ul = document.querySelector('ul');
+let url = `https://api.unsplash.com/photos/?client_id=O6-HgHCU6Jg_aNZvpAHOAACPMyV5RZ4RiEo5tvgg9vY`;
+let searchURL = (query) => `https://api.unsplash.com/search/photos/?query=${query}&client_id=O6-HgHCU6Jg_aNZvpAHOAACPMyV5RZ4RiEo5tvgg9vY`;
+
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => resolve(JSON.parse(xhr.response));
+        xhr.onerror = () => reject(`Something went wrong!`);
+        xhr.send();
+    });
+}
+
 
 function displayUI(data) {
     ul.innerHTML = "";
-    data.results.forEach(elm => {
+    data.forEach((elm) => {
         let li = document.createElement('li');
         let img = document.createElement('img');
-        img.src = elm.urls.small;
+        img.src = elm.urls.thumb;
         li.append(img);
         ul.append(img);
     });
 };
 
+fetch(url).then(displayUI).catch((error) => console.log(error));
+
+
 function handleInput(event) {
     if(event.keyCode === 13) {
-        fetch(event);
+        fetch(searchURL(input.value)).then((searchResult) => {
+            displayUI(searchResult.results);
+        }).catch((error) => console.log(error));
+        input.value = "";
     }
 }
-
-function fetch(event) {
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        let url = `https://api.unsplash.com/search/photos?query=${event.target.value}&client_id=O6-HgHCU6Jg_aNZvpAHOAACPMyV5RZ4RiEo5tvgg9vY`;
-        xhr.open("GET", url);
-        xhr.onload = () => {
-            let searchData = resolve(JSON.parse(xhr.response));
-            // let searchData = JSON.parse(xhr.response);
-            console.log(searchData);
-            displayUI(searchData);
-        }
-        xhr.onerror = () => {
-            reject(`Something went wrong!`);
-        }
-        xhr.send();
-        event.target.value = "";
-    })
-}
-
-
-
 
 input.addEventListener("keyup", handleInput);
